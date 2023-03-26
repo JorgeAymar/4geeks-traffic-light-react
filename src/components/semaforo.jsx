9cfc7b048b4623f2d740b5637f1f5dc0f3422e3b
Jorge Sánchez Aymar <jorge.s.aymar@gmail.com>
2023-03-26 17:53:10 +0000

    ultima atualización con mejoras

--- src/components/semaforo.jsx
+++ src/components/semaforo.jsx
@@ -1,46 +1,93 @@
 import React, { useState, useEffect } from 'react';
-import { Container, Row, Col } from 'react-bootstrap';
+import { Container, Row, Col, Button } from 'react-bootstrap';
 
 
 const Semaforo = () => {
-    const [lightState, setLightState] = useState('red');
-  
-    useEffect(() => {
-      const timer = setInterval(() => {
-        switch (lightState) {
-          case 'red':
-            setLightState('green');
-            break;
-          case 'green':
-            setLightState('yellow');
-            break;
-          case 'yellow':
-            setLightState('red');
-            break;
-          default:
-            setLightState('red');
-        }
-      }, 3000);
-  
-      return () => {
-        clearInterval(timer);
-      };
-    }, [lightState]);
-  
-    return (
-      <Container>
-        <Row className="justify-content-md-center">
-          <Col md="auto">
-            <div className="light-container">
-              <div className={`light ${lightState === 'red' ? 'red' : 'red-off'}`}></div>
-              <div className={`light ${lightState === 'yellow' ? 'yellow' : 'yellow-off'}`}></div>
-              <div className={`light ${lightState === 'green' ? 'green' : 'green-off'}`}></div>
-            </div>
-          </Col>
-        </Row>
-      </Container>
-    );
+  const [luzActiva, setLuzActiva] = useState('');
+  const [activo, setActivo] = useState(false);
+  const [intervalId, setIntervalId] = useState(null);
+  const [luzPurpura, setLuzPurpura] = useState(false);
+
+  const cambiarLuzActiva = (color) => {
+    setLuzActiva(color);
   };
-  
-  export default Semaforo;
-  
+
+  const luzClassName = (color) => {
+    return `luz ${color} ${luzActiva === color ? 'activa' : ''}`;
+  };
+
+  const activarSemaforo = () => {
+    if (!activo) {
+      setActivo(true);
+      const id = setInterval(() => {
+        setLuzActiva((prevLuz) => {
+          switch (prevLuz) {
+            case 'rojo':
+              return 'amarillo';
+            case 'amarillo':
+              return 'verde';
+            case 'verde':
+            default:
+              return 'rojo';
+          }
+        });
+      }, 3000);
+      setIntervalId(id);
+    }
+  };
+
+  const desactivarSemaforo = () => {
+    if (activo) {
+      setActivo(false);
+      clearInterval(intervalId);
+      setIntervalId(null);
+      setLuzActiva('');
+    }
+  };
+
+  useEffect(() => {
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, [intervalId]);
+
+  const alternarLuzPurpura = () => {
+    setLuzPurpura(!luzPurpura);
+  };
+
+  return (
+    <Container>
+      <Row className="justify-content-md-center">
+        <Col md="auto">
+          <div className="light-container">
+            <div className={luzClassName('rojo')} onClick={() => cambiarLuzActiva('rojo')}></div>
+            <div className={luzClassName('amarillo')} onClick={() => cambiarLuzActiva('amarillo')}></div>
+            <div className={luzClassName('verde')} onClick={() => cambiarLuzActiva('verde')}></div>
+            {luzPurpura && (
+              <div
+                className={luzClassName('purpura')}
+                onClick={() => cambiarLuzActiva('purpura')}
+              ></div>
+            )}
+          </div>
+        </Col>
+      </Row>
+
+      <Row className="justify-content-md-center mt-3 p-5">
+        <div className="botones">
+          <button onClick={activarSemaforo} disabled={activo} className="btn btn-primary">
+            Activar
+          </button>
+          <button onClick={desactivarSemaforo} disabled={!activo} className="btn btn-danger">
+            Desactivar
+          </button>
+          <button onClick={alternarLuzPurpura} className="btn btn-info">
+            {luzPurpura ? 'Quitar púrpura' : 'Añadir púrpura'}
+          </button>
+        </div>
+      </Row>
+    </Container>
+  );
+};
+
+export default Semaforo;
